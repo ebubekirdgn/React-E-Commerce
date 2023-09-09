98dd09a1b14b8f4fac0b1a8b71dfc3b8bbe85ff0
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2023-09-10 00:40:02 +0300

    mongodb and redis added

--- backend/src/clients/db.js
+++ backend/src/clients/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect("mongodb://localhost:27017", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
