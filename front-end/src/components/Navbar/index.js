f119db448583fc40d6c48cc96149f1f4790a7c81
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2023-09-23 18:13:57 +0300

    add styels

--- front-end/src/components/Navbar/index.js
+++ front-end/src/components/Navbar/index.js
@@ -1,17 +1,25 @@
 import React from "react";
+import styles from "./styles.module.css" 
+
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>
-      <nav>
-        <ul>
+    <nav className={styles.nav}>
+      <div className="left">
+        <div className="logo">
+          <Link to="/">E-COMMERCE</Link>
+        </div>
+
+        <ul className="menu">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">Products</Link>
           </li>
         </ul>
-      </nav>
-    </div>
+      </div>
+
+      <div className="right">RIGHT</div>
+    </nav>
   );
 }
 
