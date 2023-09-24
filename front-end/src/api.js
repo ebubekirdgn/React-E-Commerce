4147e88fb44a7c91e63479ccb3b97ff802b0af63
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2023-09-25 00:35:36 +0300

    create ProductDetail page

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -4,3 +4,7 @@ export const fetchProductList = async () => {
     const {data} = await axios.get('http://localhost:4000/product')
     return data;
 }
+export const fetchProduct = async (id) => {
+    const {data} = await axios.get(`http://localhost:4000/product/${id}`)
+    return data;
+}
--- front-end/src/pages/Products/ProductDetail.js
+++ front-end/src/pages/Products/ProductDetail.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { Box,Text,Button } from "@chakra-ui/react";
+
+import { fetchProduct } from "../../api";
+import moment from "moment";
 
 function ProductDetail() {
-  return (
-    <div>ProductDetail</div>
-  )
+
+  const {product_id} = useParams(); // useParams ile id değerini cekip aldık
+
+  const {isLoading,isError,data} = useQuery(['product',product_id],()=>fetchProduct(product_id))
+
+  if(isLoading){
+    return <div>...Loading...</div>
+  }
+  if(isError){
+    return <div>...Error...</div>
+  }
+
+console.log(data);
+
+
+  return <div>
+    <Button colorScheme="blue" variant="solid" >Sepete Ekle</Button>
+    <Text as="h2" fontSize="2xl">{data.title}</Text>
+    <Text>{moment(data.createdAt).format('DD/MM/YYYY')}</Text>
+    <Text>{data.price}</Text>
+    <p>
+      {data.description}
+    </p>
+  </div>;
 }
 
 export default ProductDetail;
