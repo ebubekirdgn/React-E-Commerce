eb38a7c0c015c770443d1a61e638b86724495585
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2023-09-28 21:49:28 +0300

    Product detail add yarn image gallery

--- front-end/src/pages/Products/ProductDetail.js
+++ front-end/src/pages/Products/ProductDetail.js
@@ -1,36 +1,44 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
-import { Box,Text,Button } from "@chakra-ui/react";
-
+import { Box, Text, Button } from "@chakra-ui/react";
+import ImageGallery from "react-image-gallery";
 import { fetchProduct } from "../../api";
 import moment from "moment";
 
 function ProductDetail() {
+  const { product_id } = useParams(); // useParams ile id değerini cekip aldık
 
-  const {product_id} = useParams(); // useParams ile id değerini cekip aldık
+  const { isLoading, isError, data } = useQuery(["product", product_id], () =>
+    fetchProduct(product_id)
+  );
 
-  const {isLoading,isError,data} = useQuery(['product',product_id],()=>fetchProduct(product_id))
-
-  if(isLoading){
-    return <div>...Loading...</div>
+  if (isLoading) {
+    return <div>...Loading...</div>;
   }
-  if(isError){
-    return <div>...Error...</div>
+  if (isError) {
+    return <div>...Error...</div>;
   }
 
-console.log(data);
+  console.log(data);
 
-
-  return <div>
-    <Button colorScheme="blue" variant="solid" >Sepete Ekle</Button>
-    <Text as="h2" fontSize="2xl">{data.title}</Text>
-    <Text>{moment(data.createdAt).format('DD/MM/YYYY')}</Text>
-    <Text>{data.price}</Text>
-    <p>
-      {data.description}
-    </p>
-  </div>;
+  const images = data.photos.map((url) => ({ original: url }));
+  return (
+    <div>
+      <Button colorScheme="blue" variant="solid">
+        Sepete Ekle
+      </Button>
+      <Text as="h2" fontSize="2xl">
+        {data.title}
+      </Text>
+      <Text>{moment(data.createdAt).format("DD/MM/YYYY")}</Text>
+      <Text>{data.price}</Text>
+      <p>{data.description}</p>
+      <Box margin="10">
+        <ImageGallery items={images} />;
+      </Box>
+    </div>
+  );
 }
 
 export default ProductDetail;
