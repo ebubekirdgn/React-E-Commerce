09971110adc5511ab84d5663252eb147b7a6f9f4
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2023-10-01 15:02:36 +0300

    add validatons for Sign Up page

--- front-end/src/pages/Auth/Signup/index.js
+++ front-end/src/pages/Auth/Signup/index.js
@@ -1,31 +1,35 @@
 import React, { useState } from "react";
-import {
-  Flex,
-  Heading,
-  Input,
-  Button,
-  InputGroup,
-  Stack,
-  InputLeftElement,
-  chakra,
-  Box,
-  FormControl,
-  InputRightElement,
-} from "@chakra-ui/react";
+import {Flex,Heading,Input,Button,InputGroup,Stack,InputLeftElement,chakra,Box,FormControl,InputRightElement} from "@chakra-ui/react";
 import { FaUserAlt, FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
+import { useFormik } from "formik";
+import validationSchema from "./validations"
+
 
 const CFaUserAlt = chakra(FaUserAlt);
 const CFaLock = chakra(FaLock);
 
 function Signup() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+    validationSchema,
+    onSubmit: async (values, bag) => {
+      console.log(values);
+    },
+  });
+
   const [showPassword, setShowPassword] = useState(false);
   const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
 
   const handleShowClickPassword = () => setShowPassword(!showPassword);
-  const handleShowClickPasswordConfirm = () => setShowPasswordConfirm(!showPasswordConfirm);
+  const handleShowClickPasswordConfirm = () =>
+    setShowPasswordConfirm(!showPasswordConfirm);
 
   return (
-    <Flex flexDirection="column" width="full" justifyContent="center">
+    <Flex align="center" width="full" justifyContent="center">
       <Stack
         flexDir="column"
         mb="2"
@@ -33,46 +37,90 @@ function Signup() {
         alignItems="center"
       >
         <Heading>Sign Up </Heading>
-        <Box minW={{ base: "90%", md: "468px" }}>
-          <form>
-            <Stack spacing={4} p="1rem"backgroundColor="whiteAlpha.900" boxShadow="md">
-              <FormControl  mt="4">
+        <Box my={5} textAlign="center">
+          <form onSubmit={formik.handleSubmit}>
+            <Stack
+              spacing={4}
+              p="1rem"
+              backgroundColor="whiteAlpha.900"
+              boxShadow="md"
+            >
+              <FormControl >
                 <InputGroup>
-                  <InputLeftElement pointerEvents="none" children={<CFaUserAlt color="gray.300" />}/>
-                  <Input name="email" type="email" placeholder="Email Address" />
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<CFaUserAlt color="gray.300" />}
+                  />
+                  <Input
+                    name="email"
+                    placeholder="Email Address"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.email}
+                  />
                 </InputGroup>
               </FormControl>
 
-              <FormControl  mt="4">
+              <FormControl >
                 <InputGroup>
-                  <InputLeftElement pointerEvents="none" color="gray.300" children={<CFaLock color="gray.300" />}/>
-                  <Input name="password" type={showPassword ? "text" : "password"}placeholder="Password"/>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="gray.300"
+                    children={<CFaLock color="gray.300" />}
+                  />
+                  <Input
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                  />
                   <InputRightElement width="4.5rem">
-                    <Button h="1.75rem" size="sm" onClick={handleShowClickPassword}>
-                      {showPassword ? <FaEye  />  :<FaEyeSlash/>}
+                    <Button
+                      h="1.75rem"
+                      size="sm"
+                      onClick={handleShowClickPassword}
+                    >
+                      {showPassword ? <FaEye /> : <FaEyeSlash />}
                     </Button>
                   </InputRightElement>
                 </InputGroup>
               </FormControl>
 
-              <FormControl  mt="4">
-                <InputGroup> 
-                  <InputLeftElement pointerEvents="none" color="gray.300" children={<CFaLock color="gray.300" />}/>
-                  <Input name="passwordConfirm" type={showPasswordConfirm ? "text" : "password"} placeholder="Password Confirm"/>
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents="none"
+                    color="gray.300"
+                    children={<CFaLock color="gray.300" />}
+                  />
+                  <Input
+                    name="passwordConfirm"
+                    type={showPasswordConfirm ? "text" : "password"}
+                    placeholder="Password Confirm"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.passwordConfirm}
+                  />
                   <InputRightElement width="4.5rem">
-                    <Button h="1.75rem" size="sm" onClick={handleShowClickPasswordConfirm}>
-                      {showPasswordConfirm ? <FaEye  />  :<FaEyeSlash/>}
+                    <Button
+                      h="1.75rem"
+                      size="sm"
+                      onClick={handleShowClickPasswordConfirm}
+                    >
+                      {showPasswordConfirm ? <FaEye /> : <FaEyeSlash />}
                     </Button>
                   </InputRightElement>
                 </InputGroup>
               </FormControl>
-              <Button mt="4" w="full" type="submit" colorScheme='blue'>
-                Sign Up</Button>
+              <Button  w="full" type="submit" colorScheme="blue">
+                Sign Up
+              </Button>
             </Stack>
           </form>
         </Box>
       </Stack>
-     
     </Flex>
   );
 }
