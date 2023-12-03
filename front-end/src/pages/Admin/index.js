4786f0bd8c187a3c54bfc3b0007b9c3553817374
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-12-03 22:38:39 +0300

    cleaned index page code

--- front-end/src/pages/Admin/index.js
+++ front-end/src/pages/Admin/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Outlet, Route, Routes, useMatch } from "react-router-dom";
+import { Link,  Route, Routes, useMatch } from "react-router-dom";
 import "./styles.css";
 import { Box, Switch } from "@chakra-ui/react";
 
