c9f0645305434f0ae23c011de7b229a35bfda93c
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-12-10 21:02:47 +0300

    link added

--- front-end/src/pages/Admin/index.js
+++ front-end/src/pages/Admin/index.js
@@ -1,8 +1,34 @@
+import { Box, Switch } from '@chakra-ui/react'
 import React from 'react'
-
+import { Link, Outlet, Route, Routes } from 'react-router-dom'
+import "./styles.css";
 function Admin() {
   return (
-    <div>Admin</div>
+    <div>
+      <header>
+        <ul className='admin-menu'>
+          <li>
+            <Link to="home">Home</Link>
+          </li>
+          <li>
+            <Link to="products">Products</Link>
+          </li>
+          <li>
+            <Link to="orders">Orders</Link>
+          </li>
+        </ul>
+      </header>
+      <Outlet />
+      {/* <Box mt="10">
+        <Switch>
+          <Routes>
+            <Route exact path="/home" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/orders" element={<Orders />} />
+          </Routes>
+        </Switch>
+      </Box> */}
+    </div>
   )
 }
 
