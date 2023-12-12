b8223940c651d0a74a8a2b2be70c803cfd87630e
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-12-12 13:03:54 +0300

    admin orders

--- front-end/src/pages/Admin/Orders/index.js
+++ front-end/src/pages/Admin/Orders/index.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { fetchOrders } from "../../../api";
+import { useQuery } from "@chakra-ui/react";
 
 function Orders() {
+  const {isLoading,isError,data,error} = useQuery('admin:error',fetchOrders);
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (isError) {
+    return <div> Error {error.message}</div>
+  }
+  
+  console.log(data)
   return (
     <>
       <h2>Admin Orders</h2>
