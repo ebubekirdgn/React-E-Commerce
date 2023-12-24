1cbd83b8d687e930843e43552650f96529da4658
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-12-25 00:24:45 +0300

    update all package
    found 0 vulnerabilities

--- front-end/src/pages/Auth/Signin/index.js
+++ front-end/src/pages/Auth/Signin/index.js
@@ -1,19 +1,43 @@
-import {Flex,Heading,Input,Button,InputGroup,Stack,InputLeftElement,chakra,Box,FormControl, Alert} from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  InputLeftElement,
+  chakra,
+  Box,
+  FormControl,
+  Alert,
+} from "@chakra-ui/react";
 import { FaUserAlt, FaLock } from "react-icons/fa";
 import { useFormik } from "formik";
 import validationSchema from "./validations";
 import { fetchLogin } from "../../../api";
 import { useAuth } from "../../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
+import { GoogleLogin } from "react-google-login";
 
 const CFaUserAlt = chakra(FaUserAlt);
 const CFaLock = chakra(FaLock);
+const clientId = [process.env.clientId];
 
 function Signin() {
-
   const { login } = useAuth();
   const navigate = useNavigate();
+  const [name, setName] = useState();
+  const [flag, setFlag] = useState(false);
+
+  const onSuccess = (res) => {
+    setName(res.profileObj["name"]);
+    console.log("Success", res.profileObj);
+    setFlag(true);
+  };
+  const onFailure = (res) => {
+    console.log("Failed", res);
+  };
 
   const formik = useFormik({
     initialValues: {
@@ -27,9 +51,9 @@ function Signin() {
           email: values.email,
           password: values.password,
         });
-         
+
         login(loginResponse);
-        navigate('/profile');
+        navigate("/profile");
       } catch (e) {
         bag.setErrors({ general: e.response.data.message });
       }
@@ -45,62 +69,70 @@ function Signin() {
         alignItems="center"
       >
         <Heading>Sign In </Heading>
-          <Box my={5}>
-              {formik.errors.general && (
-                  <Alert status="error">{formik.errors.general}</Alert>
-              )}
-          </Box>
-          <form onSubmit={formik.handleSubmit}>
-            <Stack
-              spacing={4}
-              p="1rem"
-              backgroundColor="whiteAlpha.900"
-              boxShadow="md"
-            >
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<CFaUserAlt color="gray.300" />}
-                  />
-                  <Input
-                    name="email"
-                    placeholder="Email Address"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.email}
-                    isInvalid={formik.touched.email && formik.errors.email}
-                  />
-                </InputGroup>
-              </FormControl>
+        <Box my={5}>
+          {formik.errors.general && (
+            <Alert status="error">{formik.errors.general}</Alert>
+          )}
+        </Box>
+        <form onSubmit={formik.handleSubmit}>
+          <Stack
+            spacing={4}
+            p="1rem"
+            backgroundColor="whiteAlpha.900"
+            boxShadow="md"
+          >
+            <FormControl>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents="none"
+                  children={<CFaUserAlt color="gray.300" />}
+                />
+                <Input
+                  name="email"
+                  placeholder="Email Address"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  isInvalid={formik.touched.email && formik.errors.email}
+                />
+              </InputGroup>
+            </FormControl>
 
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    color="gray.300"
-                    children={<CFaLock color="gray.300" />}
-                  />
-                  <Input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    value={formik.values.password}
-                    isInvalid={
-                      formik.touched.password && formik.errors.password
-                    }
-                  />
-               
-                </InputGroup>
-              </FormControl>
-              <Button w="full" type="submit" colorScheme="blue">
-                Sign In
-              </Button>
-            </Stack>
-          </form>
-         
+            <FormControl>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents="none"
+                  color="gray.300"
+                  children={<CFaLock color="gray.300" />}
+                />
+                <Input
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                  isInvalid={formik.touched.password && formik.errors.password}
+                />
+              </InputGroup>
+            </FormControl>
+            <Button w="full" type="submit" colorScheme="blue">
+              Sign In
+            </Button>
+            { flag ? (
+              <h2> Hello {name } </h2>
+            ) : (
+              <GoogleLogin
+              onSuccess={onSuccess}
+              onError={onFailure}
+              clientId={clientId}
+              buttonText="Google ile Giriş Yap"
+            />
+            )
+
+            }
+          </Stack>
+        </form>
       </Stack>
     </Flex>
   );
